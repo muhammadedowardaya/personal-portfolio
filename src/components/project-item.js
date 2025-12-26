a315c6cd1d555496f5937096588b8cd5f3748035
muhammadedowardaya <muhammadedowarday4@gmail.com>
2025-12-26 16:41:38 +0700

    add project-item web component, add form style

---
+++ src/components/project-item.js
@@ -0,0 +1,90 @@
+class ProjectItem extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	static get observedAttributes() {
+		return ['title', 'description'];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (oldValue !== newValue) {
+			this.connectedCallback();
+		}
+	}
+
+	connectedCallback() {
+		const preview = this.getAttribute('preview');
+		const icon = this.getAttribute('icon');
+		const title = this.getAttribute('title');
+		const description = this.getAttribute('description');
+		const github = this.getAttribute('github');
+
+		this.shadowRoot.innerHTML = `
+            <style>
+                article.project-item {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: start;
+                    gap: 28px;
+
+                    img {
+                        height: 150px;
+                        width: 100%;
+                        object-fit: cover;
+                    }
+
+                    .project-item__info {
+                        display: flex;
+                        gap: 28px;
+
+                        img {
+                            flex-shrink: 0;
+                            width: 52px;
+                            height: 52px;
+                            object-fit: cover;
+                        }
+
+                        header {
+                            h3 {
+                                font-size: 20px;
+                                font-weight: 600;
+                            }
+                        }
+                    }
+
+                    a {
+                        font-weight: 600;
+                        text-align: center;
+                        color: #fff;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        height: 40px;
+                        width: 100%;
+                        background-color: var(--color-cta);
+
+                        &:hover {
+                            background-color: var(--color-cta-hover);
+                        }
+                    }
+                }
+            </style>
+
+             <article class="project-item">
+                <img src=${preview} alt="gif preview">
+                <div class="project-item__info">
+                    <img src=${icon} alt="icon/thumbnail website">
+                    <header>
+                        <h3>${title}</h3>
+                        <p>${description}</p>
+                    </header>
+                </div>
+                <a href=${github} aria-label="github button"><span>Source Github</span></a>
+            </article>
+        `;
+	}
+}
+
+customElements.define('project-item', ProjectItem);
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+	// Menu Button
 	const menuButton = document.getElementById('menu-button');
 	const primaryNavigation = document.getElementById('primary-navigation');
 
@@ -10,6 +11,51 @@ window.addEventListener('DOMContentLoaded', () => {
 		} else {
 			primaryNavigation.classList.add('hidden');
 			menuButton.setAttribute('aria-expanded', 'false');
+		}
+	});
+
+	// Contact Form
+	const fields = document.querySelectorAll('form.contact .field');
+
+	fields.forEach((field) => {
+		const input = field.querySelector('input');
+		const textarea = field.querySelector('textarea');
+		const label = field.querySelector('label');
+
+		if (input) {
+			input.addEventListener('focus', () => {
+				if (label) {
+					if (label.classList.contains('active') === false) {
+						label.classList.add('active');
+					}
+				}
+			});
+
+			input.addEventListener('blur', () => {
+				if (label) {
+					if (input.value === '') {
+						label.classList.remove('active');
+					}
+				}
+			});
+		}
+
+		if (textarea) {
+			textarea.addEventListener('focus', () => {
+				if (label) {
+					if (label.classList.contains('active') === false) {
+						label.classList.add('active');
+					}
+				}
+			});
+
+			textarea.addEventListener('blur', () => {
+				if (label) {
+					if (textarea.value === '') {
+						label.classList.remove('active');
+					}
+				}
+			});
 		}
 	});
 });
