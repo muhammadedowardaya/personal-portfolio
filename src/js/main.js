29d4b4bbd3b696f5059f2889311adcb63499a6da
muhammadedowardaya <muhammadedowarday4@gmail.com>
2026-01-02 10:15:21 +0700

    add scrollTo to navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+// import gsap from 'gsap';
+// import { ScrollSmoother } from 'gsap/ScrollSmoother';
+
 const localTime = document.querySelector('.address .time');
 
 function updateTime() {
@@ -10,3 +13,13 @@ function updateTime() {
 
 updateTime(); // panggil langsung biar tidak nunggu 1 detik
 setInterval(updateTime, 1000);
+
+// Scroll smoooth
+
+// gsap.registerPlugin(ScrollSmoother);
+
+// ScrollSmoother.create({
+// 	smooth: 4, // how long (in seconds) it takes to "catch up" to the native scroll position
+// 	effects: true, // looks for data-speed and data-lag attributes on elements
+// 	// smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+// });
--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -1,12 +1,24 @@
 import gsap from 'gsap';
 
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 // header-navigation animation ============================
 
-const headerNavMenuSelector = ['.navbar', '.toggle-menu__text'];
+const changeNavMenuColor = (color: string) => {
+	const headerNavMenuSelector = ['.navbar', '.toggle-menu__text'];
+	const toggleMenuButton = document.getElementById('toggle-menu__button');
+	const isOpen = toggleMenuButton?.getAttribute('aria-expanded') === 'true';
+
+	if (isOpen) return;
+
+	gsap.to(headerNavMenuSelector, {
+		color: color,
+		duration: 0.1,
+	});
+};
 
 ScrollTrigger.create({
 	trigger: '.about-me article',
@@ -14,26 +26,18 @@ ScrollTrigger.create({
 	end: 'bottom top',
 	scrub: true,
 	onEnter: () => {
-		gsap.to(headerNavMenuSelector, {
-			color: '#f7f7f7',
-		})
+		changeNavMenuColor('#f7f7f7');
 	},
 	onLeave: () => {
-		gsap.to(headerNavMenuSelector, {
-			color: '#101010',
-		})
+		changeNavMenuColor('#101010');
 	},
 	onEnterBack: () => {
-		gsap.to(headerNavMenuSelector, {
-			color: '#f7f7f7',
-		})
+		changeNavMenuColor('#f7f7f7');
 	},
 	onLeaveBack: () => {
-		gsap.to(headerNavMenuSelector, {
-			color: '#101010',
-		})
+		changeNavMenuColor('#101010');
 	},
-})
+});
 
 // primary navigation mobile toggle =======================
 
@@ -41,67 +45,80 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Menu Button
 	const toggleMenuButton = document.getElementById('toggle-menu__button');
 	const primaryNavigation = document.getElementById('primary-navigation');
-	const toggleMenuText = document.querySelector('.toggle-menu__text');
+	const navItems = document.querySelectorAll('#primary-navigation ul li a');
 
-	const menuTl = gsap.timeline({
-		defaults: {
-			duration: 0.5,
-			ease: 'power1.inOut'
-		}
-	});
+	const changeMenuTo = (condition: 'open' | 'close', onComplete?: () => void) => {
+		const toggleMenuText = document.querySelector('.toggle-menu__text');
 
-	const toggleMenuTextTl = gsap.timeline({
-		defaults: {
-			ease: 'power1.inOut',
-		}
-	});
+		const menuTl = gsap.timeline({
+			defaults: {
+				duration: 0.5,
+				ease: 'power1.inOut'
+			}
+		});
+
+		const toggleMenuTextTl = gsap.timeline({
+			defaults: {
+				ease: 'power1.inOut',
+			}
+		});
+
+		menuTl.to(primaryNavigation, {
+			top: condition === 'open' ? "0%" : "100%",
+			onComplete: () => {
+				if (onComplete) onComplete();
+			},
+		});
+		toggleMenuButton?.setAttribute('aria-expanded', condition === 'open' ? 'true' : 'false');
+		toggleMenuTextTl.to(toggleMenuText, {
+			fontSize: '0px',
+			padding: 0,
+			onComplete: () => {
+				if (toggleMenuText) {
+					toggleMenuText.textContent = condition === 'open' ? 'CLOSE' : 'MENU';
+				}
+			}
+		}).to(toggleMenuText, {
+			fontSize: '16px',
+			padding: '0px 4px',
+			delay: 0.3
+		});
+
+		return menuTl;
+	};
 
 	toggleMenuButton?.addEventListener('click', () => {
 
 		const isOpen = toggleMenuButton.getAttribute('aria-expanded') === 'true';
-		console.info(`isOpen = ${isOpen}`);
 
 		if (isOpen) {
-			menuTl.to(primaryNavigation, {
-				top: "100%",
-			});
-			toggleMenuButton.setAttribute('aria-expanded', 'false');
-			toggleMenuTextTl.to(toggleMenuText, {
-				fontSize: '0px',
-				padding: 0,
-				onComplete: () => {
-					if (toggleMenuText) {
-						toggleMenuText.textContent = 'MENU';
-					}
-				}
-			}).to(toggleMenuText, {
-				fontSize: '16px',
-				padding: '0px 4px',
-				delay: 0.3
-			})
+			changeMenuTo('close');
 		} else {
-			console.info('buka menu')
-			menuTl.to(primaryNavigation, {
-				top: "0%",
-			});
-			toggleMenuButton.setAttribute('aria-expanded', 'true');
-
-			toggleMenuTextTl.to(toggleMenuText, {
-				fontSize: '0px',
-				padding: 0,
-				onComplete: () => {
-					if (toggleMenuText) {
-						toggleMenuText.textContent = 'CLOSE';
-					}
-				}
-			}).to(toggleMenuText, {
-				fontSize: '16px',
-				padding: '0px 4px',
-				delay: 0.3
-			})
+			changeMenuTo('open');
 		}
 
-		console.info(`aria-expanded = ${toggleMenuButton.getAttribute('aria-expanded')}`);
+	});
+
+	navItems.forEach((item) => {
+		console.info('navItem');
+
+		item.addEventListener('click', (e) => {
+			e.preventDefault();
+
+			const target = item.getAttribute('href');
+
+			changeMenuTo('close', () => {
+				gsap.to(window, {
+					scrollTo: {
+						y: `${target}`,
+						offsetY: 50
+					},
+					duration: 1,
+					ease: 'power1.inOut',
+					delay: 0.2
+				});
+			});
+		});
 	});
 
 });
